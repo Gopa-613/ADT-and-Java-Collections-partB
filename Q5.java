d3b47422858f3a221687c812279724008a778ca2
GOPA BHATTACHERJEE <104779240+Gopa-613@users.noreply.github.com>
2023-04-13 21:05:22 +0530

    LinkedHashSet problem

---
+++ Q5.java
@@ -0,0 +1,17 @@
+/*Write a program to create a linked hash set of type double insert some
+element into it and display it.
+*/
+import java.util.*;
+public class Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LinkedHashSet<Double> l = new LinkedHashSet<Double>();
+l.add(34.5);
+l.add(35.7);
+l.add(23.6);
+l.add(77.8);
+System.out.println("The Linked hash set contains the element : "+l);
+	}
+
+}
