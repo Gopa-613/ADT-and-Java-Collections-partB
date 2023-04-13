4bf999e77375c4348070dcdc1c9d659a2f60afa2
GOPA BHATTACHERJEE <104779240+Gopa-613@users.noreply.github.com>
2023-04-13 21:04:48 +0530

    HashSet problem

---
+++ Q4.java
@@ -0,0 +1,16 @@
+/*Write a program to create a hash set of type string insert some element
+into it and display it.*/
+import java.util.*;
+public class Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HashSet<String> h = new HashSet<String>();
+h.add("Apple");
+h.add("Mango");
+h.add("Kiwi");
+h.add("berry");
+System.out.println("The HashSet contains : "+h);
+	}
+
+}
